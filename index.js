23004cd1609f3c8a7e474abaaf3b0061b784c8ea
BrightStar <brightstar09031222@gmail.com>
2024-02-21 16:00:39 +1000

    config-change2

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const path = require("path");
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const PORT = 4000
 
-const app = express();
-// const corsOptions = {
-//   origin: 'http://localhost:8002',
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
 
-// Connect Database
-connectDB();
-
-// const corsOptions = {
-//   origin: "*",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
-app.use(cors("*"));
-
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 })
-// Init Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// Def/ine Routes
-app.use("/api/user", require("./routes/api/users"));
-app.use("/api/vehicle", require("./routes/api/vehicle"));
 
-// app.use('/', express.static(path.join(__dirname, 'build')));
 
-// if (process.env.NODE_ENV === "production") {
-// Set static folder
-// app.use(express.static("build"));
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
-// }
-
-const PORT = process.env.PORT || 5005;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-module.exports = app;
+// Export the Express API
+module.exports = app
