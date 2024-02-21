4ca815c2529502af8943d31103c4472ac6517a2b
BrightStar <brightstar09031222@gmail.com>
2024-02-22 04:31:19 +1000

    production

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ app.use("/api/vehicle", require("./routes/api/vehicle"));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// if (process.env.NODE_ENV === "production") {
-// Set static folder
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../BAZAR_CAR_frontend/dist", "index.html"));
-});
-
-
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
