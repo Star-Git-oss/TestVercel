1bc91c44886fe2c7f98caba95761e2a48af89deb
BrightStar <brightstar09031222@gmail.com>
2024-02-22 04:38:14 +1000

    backend

--- index.js
+++ index.js
@@ -6,36 +6,9 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
-// require("./config/db");
-// const corsOptions = {
-//   origin: 'http://localhost:8002',
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-
-// Connect Database
-
-// const corsOptions = {
-//   origin: "*",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
 app.use(cors("*"));
 
-app.get("/", (req, res) => {
-  res.json("Kmong");
-  const MONGODB_URI =
-    "mongodb+srv://yakiv390497:N4gkZwUKD2GahJVA@cluster0.mem6wir.mongodb.net/";
-
-  const connectDB = mongoose
-    .connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => res.status(200).json("Welcome, your app is working well"))
-    .catch((err) => res.status(200).json("Error"));
-});
+connectDB()
 // Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,16 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", require("./routes/api/users"));
 app.use("/api/vehicle", require("./routes/api/vehicle"));
 
-// app.use('/', express.static(path.join(__dirname, 'build')));
-
-// if (process.env.NODE_ENV === "production") {
-// Set static folder
-// app.use(express.static("build"));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
-// }
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5005;
 
